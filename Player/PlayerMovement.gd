b0f97f920c66c219cc5228fa4df638a0a7c92f7c
DrDoughnuts <115197311+DrDoughnuts@users.noreply.github.com>
2024-08-01 14:23:51 -0400

    Placeholder Player and Player Movement
    For creating level design example scenes.

---
+++ Player/PlayerMovement.gd
@@ -0,0 +1,13 @@
+extends Node2D
+@export var move_speed: float = 250.0
+@onready var player: CharacterBody2D = get_parent()
+
+
+func _physics_process(delta):
+	var horizontal := Input.get_axis("move_left", "move_right")
+	var vertical := Input.get_axis("move_up", "move_down")
+	
+	player.velocity = Vector2(horizontal, vertical).normalized() * move_speed
+	print(player.velocity.length())
+	
+	player.move_and_slide()
