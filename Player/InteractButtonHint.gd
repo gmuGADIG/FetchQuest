216f4696ff0f7b5a037f5fc2cfcde9ebdea25997
DrDoughnuts <115197311+DrDoughnuts@users.noreply.github.com>
2024-08-02 19:03:15 -0400

    Fix Text Bubble Name Label Color

--- Player/InteractButtonHint.gd
+++ Player/InteractButtonHint.gd
@@ -3,7 +3,7 @@ extends Sprite2D
 @onready var controller_image = preload("res://Player/Placeholder_Interact_Button_Controller.png")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	visible = get_parent().canInteract && not get_parent().inDialogue
 	if ControllerManager.isControllerMode():
 		texture = controller_image
