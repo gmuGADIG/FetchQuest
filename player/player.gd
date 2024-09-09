145fff41b089d69e6d5ba0282e41ac7c9ae3b88e
Michael Campbell <rainbow@portal2d.xyz>
2024-09-09 17:49:44 -0400

    feat: player

---
+++ player/player.gd
@@ -0,0 +1,18 @@
+class_name Player extends CharacterBody2D
+
+@export var move_speed: float = 500.0
+
+## Returns a normalized vector in the direction the player is aiming
+func get_aim() -> Vector2:
+	match ControllerManager.mode:
+		ControllerManager.ControlMode.KEYBOARD_MODE:
+			return (get_global_mouse_position() - global_position).normalized()
+		ControllerManager.ControlMode.CONTROLLER_MODE:
+			return Input.get_vector("look_left", "look_right", "look_up", "look_down")
+		_:
+			assert(false) # we shouldn't be here!
+			return Vector2.ZERO
+
+func _physics_process(_delta: float) -> void:
+	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * move_speed
+	move_and_slide()
