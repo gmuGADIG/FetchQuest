63cd8b1c8ad0eb2c9b130cde5bbfed08a36bdc65
KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>
2024-09-18 20:22:52 -0400

    init

---
+++ player/speak.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+	
+func speak() -> void:
+	pass
