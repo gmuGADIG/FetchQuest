5c5b935cb325e5bedb672ec998d8e9f79b3e8b1b
Caleb-Augustine <Calebaugustine117@gmail.com>
2024-09-18 21:30:30 -0400

    Opition Menu
    Ideal Opition Menu

---
+++ Opitions/Volume_Controller.gd
@@ -0,0 +1,13 @@
+extends HSlider
+
+@export var audio_bus_name := "Master"
+
+@onready var _bus := AudioServer.get_bus_index(audio_bus_name)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	value = db_to_linear(AudioServer.get_bus_volume_db((_bus)))
+
+
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(_bus, linear_to_db(value))
