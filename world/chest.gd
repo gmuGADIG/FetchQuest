a16bb26e2c2af1b33988a963fe52ab8a8015986c
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-09-18 20:59:12 -0400

    expanded on chest opening function

--- world/chest.gd
+++ world/chest.gd
@@ -2,21 +2,40 @@ extends Sprite2D
 
 # Properties to fine-tune the chest
 var opened := false
+var player_instance : Player
+var collider : Area2D
+
 @export var closed_texture : CompressedTexture2D
 @export var opened_texture : CompressedTexture2D
 
+# Assign one item for it to drop always,
+# or assign multiple for a random chance drop, if needed.
+@export var loot_table : Array
+@export var min_distance := 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	player_instance = get_tree().get_nodes_in_group("Player")[0]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if (Input.is_action_just_pressed("interact") and
+	position.distance_to(player_instance.position) < min_distance):
+		open_chest()
 
-
-# Opens the chest
+# Opens the chest.
 func open_chest() -> void:
+	
 	# If the chest was already opened, then forgettaboutit
 	if (opened):
+		print("Already opened!")
 		return
+	
+	# Otherwise, let's game.
+	opened = true
+	print("Opening chest: ", self)
+	texture = opened_texture
+	
+	var item : Variant = loot_table.pick_random()
+	
+	print(item)
