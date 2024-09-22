7b36363495837201312a97b490d6160b03428576
Michael Campbell <rainbow@portal2d.xyz>
2024-09-22 16:31:16 -0400

    refactor: convert chest to an area2d
    refactor: store sprites as children and change visibility instead
    
    move demo items to test scenes folder

--- world/chest/chest.gd
+++ world/chest/chest.gd
@@ -1,44 +1,40 @@
-extends Sprite2D
+extends Area2D
 
-# Properties to fine-tune the chest
 var opened := false
-var player_instance : Player
-var collider : Area2D
-
-@export var closed_texture : CompressedTexture2D
-@export var opened_texture : CompressedTexture2D
+var player: Player
 
 # Assign one item for it to drop always,
 # or assign multiple for a random chance drop, if needed.
 @export var loot_table : Array[PackedScene]
-@export var min_distance := 100
 
-# Called when the node enters the scene tree for the first time.
+@onready var closed_sprite: Sprite2D = %ClosedSprite
+@onready var open_sprite: Sprite2D = %OpenSprite
+
 func _ready() -> void:
-	player_instance = get_tree().get_nodes_in_group("Player")[0]
+	player = Player.instance
+	assert(player != null, "Player does not exist in the scene!")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if (Input.is_action_just_pressed("interact") and
-	position.distance_to(player_instance.position) < min_distance):
+	# NOTE: might be better to handle input in the player instead?
+	if Input.is_action_just_pressed("interact") and player in get_overlapping_bodies():
 		open_chest()
 
-# Opens the chest.
+## Opens the chest.
 func open_chest() -> void:
-	
 	# If the chest was already opened, then forgettaboutit
-	if (opened):
+	if opened:
 		print("Already opened!")
 		return
 	
 	# Otherwise, let's game.
 	opened = true
-	print("Opening chest: ", self)
-	texture = opened_texture
+	print("Opening chest: ", get_path())
+	closed_sprite.visible = false
+	open_sprite.visible = true
 	
 	# Picks a random item from the list and spawns it at the chest.
 	var item : PackedScene = loot_table.pick_random()
 	var spawned := item.instantiate()
-	add_sibling(spawned)
 	spawned.position = position
+	add_sibling(spawned)
 	
