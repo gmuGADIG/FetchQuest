8a7555ff316e36722c658a571641924e921efbc4
Justin1L8 <justin.w.langdon@gmail.com>
2024-09-22 17:05:40 -0400

    Refactored sword screen shake
    The sword screen shake was overly coupled with the sword; the code could not be re-used for any other screen shake.
    It was also confusing for the sword to be changing the camera like that. The bulk of the code has been moved to the a script attached to the camera (see world/camera/main_cam).

---
+++ sword/sword_screen_shake.tres.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var thrown_sword: ThrownSword
+@export var shake_strength: float
+@export var shake_duration: float
+
+func _ready() -> void:
+	thrown_sword.sword_bounced.connect(_on_sword_bounced)  # Connect the bounce signal to shake the camera
+
+func _on_sword_bounced(_intensity: float) -> void:
+	MainCam.shake(shake_strength, shake_duration)
