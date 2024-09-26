367ed43d272c3c8cc2c7612bf726f236fb6a75d6
Carlin Jasper <cjasper@gmu.edu>
2024-09-25 21:05:36 -0400

    Red Coins work!
    Pick them up, tells you how many you picked up. Reveals item at the end. Not that flashy but it works

--- DungeonPuzzles/red_coin_(area_2d).gd
+++ DungeonPuzzles/red_coin_(area_2d).gd
@@ -1,9 +1,11 @@
 extends Area2D
-
+##Make sure to have a RedCoinManager in your scene!
 @onready var red_coin_manager := $"../RedCoinManager"
+@onready var my_label := $Label
+@export var label_visible_time: float = 1.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	red_coin_manager._addRedCoin()
+	red_coin_manager._addRedCoin() #tells Manager it exist
 	pass # Replace with function body.
 
 
@@ -16,9 +18,17 @@ func _on_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("Player")):
 		#sound plays update UI?
 		print("Picked up a coin")
-		red_coin_manager._subtractRedCoin()
+		var coin_num: int = red_coin_manager._subtractRedCoin() 
+		#tells manager its been picked up and gets its number in return
+		
+		my_label.text = str(coin_num)
+		#"deletes" coin
+		$CollisionShape2D.queue_free()
+		$Sprite2D.queue_free()
+		#waits a time then deletes entire object
+		await get_tree().create_timer(label_visible_time).timeout
 		queue_free()
 	else:
 		print("That isn't the player")
-	
 	pass # Replace with function body.
+	
--- DungeonPuzzles/red_coin_manager.gd
+++ DungeonPuzzles/red_coin_manager.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 ##How it works: First place reward in scene, assign it in the inspector
-# on start it will hide the reward and see how many redCoins there are
-# when all the red coins are picked up it will unhide the reward.
+## on start it will hide the reward and see how many redCoins there are
+## when all the red coins are picked up it will unhide the reward.
 
 var num_red_coins :int
+var coins_picked_up: int
 #maybe add var for number of coins picked up for UI
 @export var reward : Node2D
 
@@ -12,6 +13,7 @@ var num_red_coins :int
 func _ready() -> void:
 	reward.set_process(false)
 	reward.set_physics_process(false)
+	reward.get_node("PickupArea").monitoring = false
 	reward.visible = false
 	pass # Replace with function body.
 
@@ -21,14 +23,16 @@ func _process(delta: float) -> void:
 	pass
 	
 func _addRedCoin() -> void:
-	num_red_coins += 1
+	num_red_coins += 1 #counting the coins in the scene
 	print("There are " + str(num_red_coins) + " red coins")
 	pass
 
-func _subtractRedCoin() -> void:
-	num_red_coins -= 1
-	if(num_red_coins <= 0):
+func _subtractRedCoin() -> int:
+	num_red_coins -= 1 #Down one coin
+	coins_picked_up += 1 #this is the nth coin picked up
+	if(num_red_coins == 0):
 		_allRedCoins()
+	return coins_picked_up
 	pass
 	
 func _allRedCoins() -> void:
@@ -36,5 +40,7 @@ func _allRedCoins() -> void:
 	#play sound yipee!
 	reward.set_process(true)
 	reward.set_physics_process(true)
+	reward.set_process_input(true)
+	reward.get_node("PickupArea").monitoring = true
 	reward.visible = true
 	pass
