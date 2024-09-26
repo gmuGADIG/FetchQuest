613409cb19ba3c571a0980b8e32468500cd0a72a
ben <bwall4@gmu.edu>
2024-09-25 21:17:24 -0400

    Edit options menu

--- ui/options/volume_controller.gd
+++ ui/options/volume_controller.gd
@@ -4,11 +4,13 @@ extends HSlider
 
 @onready var _bus := AudioServer.get_bus_index(audio_bus_name)
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Make sure we have a range of 0-1 for linear_to_db() in _on_value_changed().
+	min_value = 0
+	max_value = 1
+	# Make our step something nice and small for small increments in volume.
+	step = 0.001
 	value = db_to_linear(AudioServer.get_bus_volume_db((_bus)))
-
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(_bus, linear_to_db(value))
-	#print("Bus '%s' has volume %s" % [audio_bus_name, value])
