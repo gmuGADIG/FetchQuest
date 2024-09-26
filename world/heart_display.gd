46ac3e421359eaebb2fb6d6b3ba40818b8018e45
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-09-25 20:52:17 -0400

    further Heart work
    created heart packedscene
    also made heart amount responsive to current player health

--- world/heart_display.gd
+++ world/heart_display.gd
@@ -2,18 +2,23 @@ extends HBoxContainer
 
 var player : Player
 
+@export var heart : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = Player.instance
 	assert(player != null, "Player does not exist in the scene!")
-	
-	player.player_hurt.connect(onHurt)
-
+	update_heart_display()
+	player.player_hurt.connect(update_heart_display)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-func onHurt() -> void:
-	print(player.health)
+
+
+func update_heart_display() -> void:
+	for _child in get_children():
+		remove_child(_child)
+		_child.queue_free()
+	for h in player.health:
+		add_child(heart.instantiate())
