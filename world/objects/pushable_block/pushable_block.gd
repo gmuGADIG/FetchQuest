12cebb0605eb60af61a9383e1339a9a14f169039
Justin1L8 <justin.w.langdon@gmail.com>
2024-09-29 17:22:21 -0400

    fixed pushing blocks from sides + enabled collision with terrain
    When you walked against the side of a pushable block, it would pull it sideways with you. This has been fixed.
    Also, pushable blocks should have collision with terrain and other pushable blocks. This was added by changing the collision mask.

--- world/objects/pushable_block/pushable_block.gd
+++ world/objects/pushable_block/pushable_block.gd
@@ -1,32 +1,30 @@
 extends CharacterBody2D
 
 @export var move_speed: float = 250.0
-@export var push_velocity: Vector2 = Vector2.ZERO
 
-func _physics_process(_delta: float) -> void:	
+func _physics_process(delta: float) -> void:	
 	# Update push velocity; search for overlapping player bodies that would exert a force
 	var overlapping_bodies: Array[Node2D] = $Area2D.get_overlapping_bodies()
 	for body in overlapping_bodies:
 		var player := body as Player
 		if player != null:
 			# Player found; get a pushing force from their velocity
-			var plr_force: Vector2 = player.velocity.normalized()
+			var push_direction: Vector2 = player.velocity.normalized()
 			
 			# Force the block to move in a cardinal direction by 
 			# zeroing out the lesser component of its velocity
-			if abs(plr_force.x) > abs(plr_force.y):
-				plr_force.y = 0;
+			if abs(push_direction.x) > abs(push_direction.y):
+				push_direction.y = 0;
 			else:
-				plr_force.x = 0;
+				push_direction.x = 0;
 			
 			# Push velocity isn't changed if the push force points towards the player (And thus, not the block)
 			# Prevents issues with the block being sticky and getting pushed when the player moves away
-			var to_plr: Vector2 = position.direction_to(player.position)			
-			if to_plr.dot(plr_force) < -0.1:
+			var direction_to_player: Vector2 = position.direction_to(player.position)			
+			if direction_to_player.dot(push_direction) < -0.7:
 				# Set push velocity
-				push_velocity = plr_force.normalized() * move_speed
+				velocity = push_direction.normalized() * move_speed
 	
 	# Move based on push velocity
-	push_velocity *= 0.9 # So the block slows down when not pushed
-	velocity = push_velocity
+	velocity *= 0.9 # So the block slows down when not pushed
 	move_and_slide()
