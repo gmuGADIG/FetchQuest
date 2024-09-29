78ec34f59fe3c2bf4d016d4465fa4eb6164f8886
Justin1L8 <justin.w.langdon@gmail.com>
2024-09-29 15:46:12 -0400

    reset main scene + fixed sword reparenting crash

--- player/sword/thrown_sword.gd
+++ player/sword/thrown_sword.gd
@@ -54,7 +54,7 @@ func _physics_process(delta: float) -> void:
 	var cos_theta: float = player_dist.dot(old_player_dist) / (player_dist.length() * old_player_dist.length())
 	if player_dist.length() <= 100 and cos_theta < 0 and returning:
 		for item: Node in held_items:
-			item.reparent(get_parent())
+			if item != null: item.reparent(get_parent())
 		queue_free()
 
 	# Handle movement and acceleration while returning or still thrown
