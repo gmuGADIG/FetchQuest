4abae6172f6cb6adcf19f81b752cf6649c73e809
Cowmillesse <camillafindley@gmail.com>
2024-10-02 20:13:35 -0400

    Doors work
    Doors now check for keys and follow the funny meme with lightning if the player has a key.

--- world/objects/locked_door/locked_door.gd
+++ world/objects/locked_door/locked_door.gd
@@ -1,13 +1,6 @@
-class_name LockedDoor
+class_name LockedDoor extends Node2D
 
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		if PlayerInventory.use_door_key():
+			queue_free()
