088130b7851161bd8712ba2bf58c73f4cafab91c
Tushar <45638547+MistOfJudgement@users.noreply.github.com>
2024-10-02 20:10:56 -0400

    Added sprite

--- world/switch/switch.gd
+++ world/switch/switch.gd
@@ -13,15 +13,13 @@ enum SWITCH_TYPE {
 @export var type: SWITCH_TYPE = SWITCH_TYPE.OneShot
 @export var active_time: float = 0.0
 var _timer: Timer
+var _on_sprite: Sprite2D
+var _off_sprite: Sprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_timer = get_node("Timer")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
+	_on_sprite = get_node("OnSprite")
+	_off_sprite = get_node("OffSprite")
 
 func _on_area_entered(area: Area2D) -> void:
 	trigger_switch()
@@ -33,19 +31,18 @@ func trigger_switch() -> void:
 	match(type):
 		SWITCH_TYPE.OneShot:
 			if not activated:
-				activated = true
-				switch_activated.emit()
+				activate()
+				
 				
 		SWITCH_TYPE.Toggle:
-			activated = not activated
 			if activated:
-				switch_activated.emit()
+				deactivate()
 			else:
-				switch_deactivated.emit()	
+				activate()
+				
 		SWITCH_TYPE.Timed:
 			if not activated:
-				activated = true
-				switch_activated.emit()
+				activate()
 				_timer.start(active_time)
 			else:
 				#restart timer
@@ -53,7 +50,18 @@ func trigger_switch() -> void:
 				_timer.start(active_time)
 
 
+func activate() -> void:
+	activated = true
+	switch_activated.emit()
+	_on_sprite.visible = true
+	_off_sprite.visible = false
+	
+func deactivate() -> void:
+	activated = false
+	switch_deactivated.emit()
+	_on_sprite.visible = false
+	_off_sprite.visible = true
+
 func _on_timer_timeout() -> void:
 	if activated:
-		activated = false
-		switch_deactivated.emit()
+		deactivate()
