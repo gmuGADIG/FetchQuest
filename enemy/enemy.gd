8a14e4aed90a68cdbab8c1f726d7f9b571346518
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2024-10-02 20:18:08 -0400

    on_enemy_death
    Created a new function `on_enemy_death` to handle logic in the event an enemy dies. So far:
    - Random (exposed) chance is implemented.

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -7,6 +7,8 @@ class_name Enemy extends CharacterBody2D
 @export var max_health: int = 3
 @onready var health := max_health
 
+@export var enemy_drop_chance_percent: int = 50 # 50 = 50% chance
+
 ## Damages the enemy, killing it if its health drops below zero.
 ## This should be called by the player's attacks when they hit an enemy.
 func hurt(damage_event: DamageEvent) -> void:
@@ -18,4 +20,15 @@ func hurt(damage_event: DamageEvent) -> void:
 	print("Enemy.gd: Health of '%s' was lowered to %s/%s" % [get_path(), health, max_health])
 
 	if health <= 0:
+		on_enemy_death()
 		queue_free()
+
+## Function to call upon death of enemy
+func on_enemy_death() -> void:
+	# if the chance fails, bail out of the function and do nothing
+	if (randi_range(1,100) > enemy_drop_chance_percent): return
+	
+	# otherwise, let's check eligibility to drop items
+	# if the player has max hearts, then dont do hearts
+	# if the player has max bombs or havent unlocked them yet, then dont do bombs
+	# in that case, stamina will always be available
