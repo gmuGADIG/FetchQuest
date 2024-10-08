a6317befc4f1cc7b6a78f7b6cfba93efb738916f
K <redknclarke@gmail.com>
2024-10-07 20:59:03 -0400

    got rid of redundant calculations

--- enemy/enemy_player_detection.gd
+++ enemy/enemy_player_detection.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 ## The distance in pixels that the enemy can "see" the player from
 @export var detectable_distance:float = 250
@@ -12,22 +12,27 @@ var detecting:bool = true;
 ## The distance from our enemy to the player
 var distance_to_player:float = 0
 
+## Whether or not the player is within the detectable radius
+var player_in_detection_zone:bool = false
+
 ## A signal emitted once the player has been "seen"
 signal player_detected;
 
 func _ready() -> void:
 	## Set the Area2D size and raycast length to be our desired measurements
-	$DetectionZone.shape.radius = detectable_distance
 	raycast.target_position.x = detectable_distance
 
 func _physics_process(delta: float) -> void:
 	## Update the distance to the player
 	distance_to_player = abs(global_position.distance_to(Player.instance.global_position))	
-
-	## Make sure we still want to detect the player
-	if detecting:
-		## Check both the area2d and the raycast for the player's presence
-		if overlaps_body(Player.instance) and (raycast.get_collider() as Player == Player.instance):
+	
+	## Update whether the player is in the detection zone or not
+	player_in_detection_zone = distance_to_player <= detectable_distance
+	
+	## Make sure we still want to detect the player and that it is close enough
+	if detecting and player_in_detection_zone:
+		## Check the raycast for the player's presence
+		if (raycast.get_collider() as Player == Player.instance):
 				## Emit the signal upon detecting the player
 				player_detected.emit()
 
