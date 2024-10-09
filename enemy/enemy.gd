aa07d67ec4d606ea906f81241bc3ebc53e5ffaaa
Justin1L8 <justin.w.langdon@gmail.com>
2024-10-09 16:33:17 -0400

    fixed merge issues + uid stuff

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -61,10 +61,10 @@ func on_death() -> void:
 	# add bombs, health, and stamina to the list of possible drops, after checking if they're eligible
 	var eligible_pickup_paths: Array[String]
 	if (Player.instance.health < Player.instance.max_health):
-		eligible_pickup_paths.append("uid://ba67nujgxs2xm") # health
+		eligible_pickup_paths.append("res://world/environment/pickups/pickup_health.tscn") # health
 	if (PlayerInventory.bombs < PlayerInventory.max_bombs):
-		eligible_pickup_paths.append("uid://bcxhlev2837g6") # bomb
-	eligible_pickup_paths.append("uid://nqdbsvt7vcge") # stamina (unconditional)
+		eligible_pickup_paths.append("res://world/environment/pickups/pickup_bomb.tscn") # bomb
+	eligible_pickup_paths.append("res://world/environment/pickups/pickup_stamina.tscn") # stamina (unconditional)
 	
 	# If, somehow, there are no eligible items, then sound the alarms and bail 
 	if (eligible_pickup_paths.is_empty()):
