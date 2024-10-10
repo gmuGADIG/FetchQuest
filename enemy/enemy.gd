46cf81f9f4cc9cf6a74322ad20238cdb942d8302
ben <bwall4@gmu.edu>
2024-10-09 21:04:58 -0400

    Implement some basic navigation logic

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -4,15 +4,11 @@
 
 class_name Enemy extends CharacterBody2D
 
-var _player: Player
-
 @export var max_health: int = 3
 @onready var health := max_health
 
-@export var movement_speed: float = 4.0
+@export var movement_speed: float = 256.0
 @onready var navigation_agent: NavigationAgent2D = get_node("NavigationAgent2D")
-
-
 
 var enemy_state := EnemyState.ROAMING
 
@@ -23,15 +19,12 @@ enum EnemyState {
 }
 
 func _ready() -> void:
-	navigation_agent.velocity_computed.connect(Callable(_on_velocity_computed))
-	_player = Player.instance
-	assert(_player != null, "No player exists in the scene!")
+	navigation_agent.velocity_computed.connect(self._on_velocity_computed)
 	actor_setup.call_deferred()
-
 
 func actor_setup() -> void:
 	await get_tree().physics_frame
-	approach(_player.global_position)
+	approach(Player.instance.global_position)
 
 func _process(delta: float) -> void:
 	match enemy_state:
@@ -41,7 +34,6 @@ func _process(delta: float) -> void:
 			_process_agressive(delta)
 		EnemyState.STUNNED:
 			_process_stunned(delta)
-	
 		
 ## Damages the enemy, killing it if its health drops below zero.
 ## This should be called by the player's attacks when they hit an enemy.
@@ -65,8 +57,6 @@ func _process_agressive(delta: float) -> void:
 func _process_stunned(delta: float) -> void:
 	pass
 	
-	
-	
 func approach(target: Vector2) -> void:
 	set_movement_target(target)
 
@@ -74,22 +64,19 @@ func set_movement_target(movement_target: Vector2) -> void:
 	navigation_agent.set_target_position(movement_target)
 
 func _physics_process(delta: float) -> void:
-# Do not query when the map has never synchronized and is empty.
-	
-	#if NavigationServer2D.map_get_iteration_id(navigation_agent.get_navigation_map()) == 0:
-		#return
-	#if navigation_agent.is_navigation_finished():
-		#return
-	#if _player == null: return
+	if navigation_agent.is_navigation_finished():
+		return
 	
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 	var new_velocity: Vector2 = global_position.direction_to(next_path_position) * movement_speed
 	if navigation_agent.avoidance_enabled:
+		# this implicitly calls _on_velocity_computed.
 		navigation_agent.set_velocity(new_velocity)
 	else:
 		_on_velocity_computed(new_velocity)
-
-
+		
+	# TODO: Set our movement target based on our enemy's specific logic.
+	set_movement_target(Player.instance.global_position)
 
 func _on_velocity_computed(safe_velocity: Vector2) -> void:
 		velocity = safe_velocity
