00cea4c15f9fd071314bb64a0747cc10a872cf1f
K <redknclarke@gmail.com>
2024-10-09 23:43:02 -0400

    comments for shield enemy

--- enemy/shield_enemy/shield_enemy.gd
+++ enemy/shield_enemy/shield_enemy.gd
@@ -1,25 +1,40 @@
 class_name ShieldEnemy extends Enemy
+
+## The speed that the enemy will move 
 @export var movement_speed:float = 300
-@export var shield_size:float = 60
+
+## The size of the shield in degrees (360 is a full shield and 0 is no shield)
+@export_range(0, 360, 0.025) var shield_size:float = 60
+
+
+# NOTE 	This movement code is taken from the test_enemy 
+#   	and will probably not stay like this forever
 func _ready() -> void:
-	# wait a single frame in case our _ready was called before the player's
+	# Wait a single frame in case our _ready was called before the player's
 	await get_tree().process_frame
 
 func _physics_process(_delta: float) -> void:
-	if Player.instance == null: return
+	if Player.instance == null: 
+		return
 
-	else:
-		var move_direction := (Player.instance.global_position - global_position).normalized()
-		velocity = move_direction * movement_speed
-		look_at(Player.instance.global_position)
+	# Move towards and look at the player 
+	var movement_direction := (Player.instance.global_position - global_position).normalized()
+	velocity = movement_direction * movement_speed
+	look_at(Player.instance.global_position)
 	move_and_slide()
-	
+
+## Called when something gets close to the enemy
 func _on_shield_body_entered(body: Node2D) -> void:
+	# Angle between the enemy and the object near the enemy
 	var angle_to_body:float = rad_to_deg(get_angle_to(body.global_position))
 
+	# Variable showing if the shield was hit (ignored by some objects like the bomb)
+	var hit_shield:bool = (-shield_size / 2) < angle_to_body and angle_to_body < (shield_size / 2)
+
+	# Deflect ThrownSwords
 	if body is ThrownSword:
-		if (-shield_size / 2) < angle_to_body and angle_to_body < (shield_size / 2):
+		if hit_shield:
 			print("shield_enemy.gd: hit shield")
 			body.return_sword()
-	# if body is bomb:
-		# let bomb through
+	# TODO if body is bomb:
+			# let bomb through
