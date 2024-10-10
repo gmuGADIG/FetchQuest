5b5cac68cd0797d70a3d5858dd8a96a065d057aa
ben <bwall4@gmu.edu>
2024-10-09 20:30:29 -0400

    enemy test nav

---
+++ enemy/test_enemy/test_enemy_nav.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+var movement_speed: float = 200.0
+
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+
+func _ready() -> void:
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	#navigation_agent.path_desired_distance = 4.0
+	#navigation_agent.target_desired_distance = 4.0
+
+	# Make sure to not await during _ready.
+	actor_setup.call_deferred()
+
+func actor_setup() -> void:
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(%Target.global_position)
+
+func set_movement_target(movement_target: Vector2) -> void:
+	navigation_agent.target_position = movement_target
+
+func _physics_process(delta: float) -> void:
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	move_and_slide()
