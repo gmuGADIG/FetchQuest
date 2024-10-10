8fbca5b9e4da769fd85184bab04c2c9bc24d3409
CTaylor <Rubiksmaster2021@gmail.com>
2024-10-10 02:04:26 -0400

    Changed Targets to a Rigid body
    Changed the targets from a 2dnode to a rigidbody. The sword now bounces off of the targets. All that is left to be added is the timer and making that targets flash and optimization.

--- test_scenes/Target_Practice/target.gd
+++ test_scenes/Target_Practice/target.gd
@@ -4,16 +4,23 @@ extends Node2D
 
 @export var speed:= 100
 @export var target_node: Node2D
+@export var collision_zone: Area2D
+@export var player: CharacterBody2D
+@export var target_collision: CollisionShape2D
+@export var sprite: Sprite2D
 
 var dir: bool = false
 var go: bool = false
 
-#func _ready() -> void:
-	# Manually connect the signal if needed
-	#self.connect("body_entered", self, "_on_Area2D_body_entered")
+func _ready() -> 	void:
+	
+	collision_zone.connect("body_shape_entered", Callable(self, "_on_body_shape_entered"))
+	collision_zone.connect("body_shape_exited", Callable(self, "_on_body_shape_exited"))
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	target_collision.global_position = sprite.global_position
 	if go == true:
 		if dir == false:
 			path_follow.progress += speed * delta
@@ -25,13 +32,11 @@ func _process(delta: float) -> void:
 				dir = false
 	
 
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("yousure?")
+func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	print("yousure?")	
 	go = true
 
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
+func _on_body_shape_exited(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
 	#shutdown method yet to be implemeted to reset everything upon exited area
 	go = false
 	path_follow.progress = 0.0
