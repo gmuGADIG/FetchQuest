5b9cf117176d696e26f5b3c5b5b5516e85382e3e
ben <bwall4@gmu.edu>
2024-10-09 20:40:50 -0400

    Mess with enemy nav

--- enemy/test_enemy/test_enemy_nav.gd
+++ enemy/test_enemy/test_enemy_nav.gd
@@ -31,4 +31,5 @@ func _physics_process(delta: float) -> void:
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 
 	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	navigation_agent.set_velocity(velocity)
 	move_and_slide()
