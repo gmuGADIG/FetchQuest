b9748d7408b1d084d054250b2a977940d43abc72
Caleb-Augustine <Calebaugustine117@gmail.com>
2024-10-09 21:28:43 -0400

    Boss Health
    Updated the boss health bar system so i am hopefully on the right track.

---
+++ test_scenes/bossHealthBar.gd
@@ -0,0 +1,25 @@
+extends ProgressBar
+
+#Grab Enemy
+@export var boss : Enemy 
+#grab value
+
+func _on_boss_health_changed() -> void:
+	value = boss.health
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	max_value = boss.max_health
+	value = boss.health
+	boss.health_changed.connect(_on_boss_health_changed)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+#on value change
+
+#reset value to enemy health
