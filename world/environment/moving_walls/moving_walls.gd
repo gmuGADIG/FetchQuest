8d7dd3b2f85f21777f509af696f3ee1d55f747ef
khalid <71997967+grmpyktn11@users.noreply.github.com>
2024-10-09 21:13:22 -0400

    Update moving_walls.gd

--- world/environment/moving_walls/moving_walls.gd
+++ world/environment/moving_walls/moving_walls.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
 @onready var path_follow : PathFollow2D = $Path2D/PathFollow2D
-@onready var moveForward : bool = true
+#Will move things following the path initially
+@onready var moveForward : bool = true 
 # How fast we want the wall to move
 @export var speed  : int = 100
+# Allows the object to bounce
 @export var bouncing : bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if bouncing:
+	if bouncing: 
 		if moveForward:
 			if path_follow.progress_ratio < 1:
 				path_follow.progress_ratio += speed * delta 
