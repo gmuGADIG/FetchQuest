0936b532c60d16449d81131604437550c1405aee
Justin1L8 <justin.w.langdon@gmail.com>
2024-10-11 16:09:23 -0400

    added types

--- reloader/reloader.gd
+++ reloader/reloader.gd
@@ -13,12 +13,12 @@ func _ready() -> void:
 	for reloadable in get_tree().get_nodes_in_group("reloads"):
 		assert(reloadable is Reloadable, "Node '%s' should not be in the 'reloads' group!" % reloadable.get_path())
 		
-		var node = reloadable.get_parent() # the Reloadable node is added as a component to the node we want to save
+		var node := reloadable.get_parent() # the Reloadable node is added as a component to the node we want to save
 		print("saving node '%s'" % node.get_path())
 		
 		# take the node out of the tree, and add it to our own list as a ReloadableNode
 		node.get_parent().remove_child(node)
-		var rel = ReloadableNode.new()
+		var rel := ReloadableNode.new()
 		rel.saved_node = node
 		rel.visible_rect = reloadable.rect
 		saves.append(rel)
@@ -37,10 +37,10 @@ func _process(delta: float) -> void:
 				get_tree().current_scene.add_child(reloadable_node.instantiate())
 	
 func _is_on_screen(rect: Rect2, margin: float = 0) -> bool:
-	var cam = get_viewport().get_camera_2d()
-	var center = cam.get_screen_center_position()
-	var size = get_viewport().get_visible_rect().size
-	var screen_rect = Rect2(center - size / 2.0, size)
+	var cam := get_viewport().get_camera_2d()
+	var center := cam.get_screen_center_position()
+	var size := get_viewport().get_visible_rect().size
+	var screen_rect := Rect2(center - size / 2.0, size)
 
 	return screen_rect.grow(margin).intersects(rect)
 
@@ -49,13 +49,13 @@ class ReloadableNode:
 	var saved_node: Node2D ## The node saved outside the scene tree
 	var instanced_node: Node2D ## Nullable reference to the node inside the tree
 
-	func instantiate():
+	func instantiate() -> Node2D:
 		assert(instanced_node == null, "Try to instantiate a ReloadableNode when an instance already exists!")
 		instanced_node = saved_node.duplicate()
 		return instanced_node
 	
-	func get_global_visible_rect():
-		var rect = visible_rect
+	func get_global_visible_rect() -> Rect2:
+		var rect := visible_rect
 		if instanced_node != null:
 			rect.position += instanced_node.global_position
 		else:
