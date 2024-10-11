d218010030d85516d7aa79319fc90a1f0320be83
Justin1L8 <justin.w.langdon@gmail.com>
2024-10-11 17:27:14 -0400

    Added houses + fixed camera smoothing bug + fixed bomb exploding player bug

--- world/decoration/decoration_randomizer.gd
+++ world/decoration/decoration_randomizer.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 @export_range(0, 1) var scale_randomness := 0.1
+@export_range(0, 1) var color_randomness := 0.2
 @export var can_flip := true
 
 func _ready() -> void:
 	scale *= 1 + randf_range(-scale_randomness, +scale_randomness)
+	modulate *= 1 + randf_range(-color_randomness, +color_randomness)
+	modulate.a = 1
 	if can_flip and randf() > 0.5: scale.x *= -1
