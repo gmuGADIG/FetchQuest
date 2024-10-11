e4f49e624415db56d4fb181aff6d226c24bdab2b
Justin1L8 <justin.w.langdon@gmail.com>
2024-10-11 16:54:38 -0400

    Fixed enemy reloader and applied it to base enemy and latest_demo_2

--- reloader/reloadable.gd
+++ reloader/reloadable.gd
@@ -1,4 +1,4 @@
 class_name Reloadable extends VisibleOnScreenNotifier2D
 
 func _ready() -> void:
-	assert(self.is_in_group("reloads"), "Reloadable at '%s' must be in the 'Reloads' group!" % get_path())
+	assert(self.is_in_group("Reloads"), "Reloadable at '%s' must be in the 'Reloads' group!" % get_path())
--- reloader/reloader.gd
+++ reloader/reloader.gd
@@ -14,14 +14,14 @@ var saves: Array[ReloadableNode] = []
 func _ready() -> void:
 	#print("reloader: starting")
 	for reloadable in get_tree().get_nodes_in_group("Reloads"):
-		assert(reloadable is Reloadable, "Node '%s' should not be in the 'reloads' group!" % reloadable.get_path())
+		assert(reloadable is Reloadable, "Reloadable node '%s' should not be in the 'Reloads' group!" % reloadable.get_path())
 		
 		# note: the Reloadable node is added as a component to the node we want to save, so we get the parent
 		var node := reloadable.get_parent()
 		#print("reloader: saving node '%s'" % node.get_path())
 		
 		# take the node out of the tree, and add it to our own list as a ReloadableNode
-		node.get_parent().remove_child(node)
+		node.get_parent().remove_child.call_deferred(node)
 		var rel := ReloadableNode.new()
 		rel.saved_node = node
 		rel.visible_rect = reloadable.rect
@@ -29,19 +29,20 @@ func _ready() -> void:
  
 func _process(delta: float) -> void:
 	for reloadable_node in saves:
-		if not reloadable_node.unloaded:
+		if not reloadable_node.loaded:
 			# node isn't loaded; handle spawning
 			if _is_on_screen(reloadable_node.get_global_visible_rect(), spawn_margin):
 				print("Loading node '%s'" % reloadable_node.saved_node.name)
 				get_tree().current_scene.add_child(reloadable_node.instantiate())
+				reloadable_node.loaded = true
 		else:
 			# node has been loaded; handle despawning
 			# note that the node instance may still be null (if e.g. an enemy died)
 			if not _is_on_screen(reloadable_node.get_global_visible_rect(), despawn_margin):
-				print("Freeing node '%s'" % reloadable_node.instanced_node.name)
+				print("Freeing node '%s'" % reloadable_node.saved_node.name)
 				if reloadable_node.instanced_node != null:
 					reloadable_node.instanced_node.queue_free()
-				reloadable_node.unloaded = true
+				reloadable_node.loaded = false
 	
 func _is_on_screen(rect: Rect2, margin: float = 0) -> bool:
 	var cam := get_viewport().get_camera_2d()
@@ -55,7 +56,7 @@ class ReloadableNode:
 	var visible_rect: Rect2 ## local visibility rect, as defined by the Reloadable node
 	var saved_node: Node2D ## The node saved outside the scene tree
 	var instanced_node: Node2D ## Nullable reference to the node inside the tree
-	var unloaded := false
+	var loaded := false
 
 	func instantiate() -> Node2D:
 		assert(instanced_node == null, "Try to instantiate a ReloadableNode when an instance already exists!")
