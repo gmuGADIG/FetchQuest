d278e2c0b126e5aaa534e7d087dc3b205f4041cb
Justin1L8 <justin.w.langdon@gmail.com>
2024-10-12 16:30:18 -0400

    Demo dungeon + title screen + shield enemy

--- ui/title/main_menu.gd
+++ ui/title/main_menu.gd
@@ -1,15 +1,4 @@
-extends Label
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+extends CanvasLayer
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
@@ -17,7 +6,7 @@ func _on_quit_pressed() -> void:
 
 
 func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://world/latest_demo.tscn")
+	get_tree().change_scene_to_file("res://world/latest_demo_2.tscn")
 	pass # Replace with function body.
 
 
