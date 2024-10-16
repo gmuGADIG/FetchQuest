40c99b4f20e00ca96fe2c35a6aab45a1f10bd203
Caleb-Augustine <Calebaugustine117@gmail.com>
2024-10-16 19:42:10 -0400

    Update bossHealthBar.gd
    Added the ability for the health bar to delete itself.

--- test_scenes/bossHealthBar.gd
+++ test_scenes/bossHealthBar.gd
@@ -18,6 +18,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if boss == null:
+		queue_free()
+		is_queued_for_deletion()
 	pass
 
 #on value change
