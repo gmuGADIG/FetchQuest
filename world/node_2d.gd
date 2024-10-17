f79002c2184e3b51ee43f248ea327ff2a8cf113c
Daringdeveloper25 <jhunt25@gmu.edu>
2024-10-16 21:27:04 -0400

    Weak Floor V1
    Made a weak floor that prints a message when the player steps on it. There is much work to be done on this

---
+++ world/node_2d.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if(body.is_in_group("Player")):
+		print("You stepped on a weak part of the floor!")
+		
+		
