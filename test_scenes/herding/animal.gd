d53410a76f95731921a1cc9aad9a0c2bb28ded85
Will Owens <wowens4@gmu.edu>
2024-10-16 21:11:52 -0400

    animal herding now functional. still to do: make the animal brain smarter, write fuller documentation

--- test_scenes/herding/animal.gd
+++ test_scenes/herding/animal.gd
@@ -1,6 +1,7 @@
 class_name Animal
 extends CharacterBody2D
 
+@export var pen: AnimalPen = null
 var speed: float = 200.0
 var direction: Vector2 = Vector2(1, 0)
 var panic: bool = false
@@ -10,8 +11,7 @@ var safe: bool = false
 @export var panic_speed: float = 600.0
 @export var panic_interval: float = 1.0
 
-@export var pen_object: AnimalPen
-
+# move into panicked state from an event at the supplied position
 func panic_from_position(pos: Vector2) -> void:
 	if (panic or safe):
 		return 
@@ -22,6 +22,7 @@ func panic_from_position(pos: Vector2) -> void:
 	# calculate a direction directly away from the supplied position
 	direction = (self.global_position - pos).normalized() * 1.0
 
+# stop panicking
 func calm() -> void:
 	panic = false
 	$"Panic Timer".stop()
@@ -29,9 +30,7 @@ func calm() -> void:
 
 func _ready() -> void:
 	direction = Vector2.from_angle(randf_range(0.0, 360.0)).normalized()
-	if (pen_object):
-		pen_object.body_entered.connect(_on_entered_safe_area)
-		pen_object.body_exited.connect(_on_leave_pen)
+	pen.add_target_animal(self)
 
 func _physics_process(delta: float) -> void:
 	if (is_on_wall()):
@@ -40,20 +39,32 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 
-func _on_brain_timer_timeout() -> void:
-	print(safe)
-	print("brain time")
-	if (panic):
-		return
-	elif (safe):
-		direction = Vector2.from_angle(randf_range(0.0, 360.0)).normalized()
-		speed = randf_range(0.8, 1.1) * safe_speed
+# brain processing when in the pen
+func think_when_safe() -> void:
+	direction = Vector2.from_angle(randf_range(0.0, 360.0)).normalized()
+	speed = randf_range(0.8, 1.1) * safe_speed
+
+# brain processing when panicking
+func think_when_panicking() -> void:
+	# don't recalculate, just go in the direction we already want to go in
+	pass
+
+# brain processing when outside the pen
+func think_when_normal() -> void:
+	direction = Vector2.from_angle(randf_range(0.0, 360.0)).normalized()
+	if (randi_range(0, 4) > 2):
+		speed = randf_range(130.0, 220.0)
 	else:
-		direction = Vector2.from_angle(randf_range(0.0, 360.0)).normalized()
-		if (randi_range(0, 4) > 2):
-			speed = randf_range(130.0, 220.0)
-		else:
-			speed = 0.0
+		speed = 0.0
+
+# determine next objective
+func _on_brain_timer_timeout() -> void:
+	if (panic):
+		think_when_panicking()
+	elif (safe):
+		think_when_safe()
+	else:
+		think_when_normal()
 
 # calm down when the panic timer expires
 func _on_panic_timer_timeout() -> void:
@@ -63,23 +74,24 @@ func _on_panic_timer_timeout() -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	panic_from_position(body.global_position)
 
-# entered the safe area
-func _on_entered_safe_area(body: Node2D) -> void:
+# called from the pen when an animal enters it for the first time
+func enter_safe_area() -> void:
 	if (safe):
 		return
-	pen_object.animal_entered(self)
 	safe = true
 	calm()
 	self.set_collision_mask_value(2, false)
 	self.set_collision_mask_value(3, false)
 	self.set_collision_mask_value(8, false)
-	direction = ((self.global_position - pen_object.global_position) * -1.0).normalized()
+	direction = ((self.global_position - pen.global_position) * -1.0).normalized()
 	speed = safe_speed
 	$"Brain Timer".stop()
 	$"Brain Restart Timer".start(0.75)
 
-func _on_leave_pen(body: Node2D) -> void:
-	direction = ((self.global_position - pen_object.global_position) * -1.0).normalized()
-
+# if the brain was shut down, restart it
 func _on_brain_restart_timer_timeout() -> void:
 	$"Brain Timer".start(2.5)
+
+# if we leave the safe area, this gets called to make us go back inside
+func retarget_safe_area() -> void:
+	direction = ((self.global_position - pen.global_position) * -1.0).normalized()
--- test_scenes/herding/animal_pen.gd
+++ test_scenes/herding/animal_pen.gd
@@ -1,14 +1,28 @@
 class_name AnimalPen extends Area2D
 
-@export var animals: Array[Animal] = []
+var animals: Array[Animal] = []
 var animals_inside: Array[Animal] = []
 var complete: bool = false
 
-func animal_entered(animal: Animal) -> void:
-	print(animals_inside)
-	if (animals_inside.has(animal)):
+func add_target_animal(animal: Animal) -> void:
+	animals.push_back(animal)
+
+func _on_body_entered(body: Node2D) -> void:
+	if (body in animals_inside):
 		return
-	animals_inside.push_back(animal)
-	if (animals_inside.size() == animals.size()):
-		complete = true
-		print("yippee!!!!!!!")
+	if not (body in animals):
+		return
+	
+	animals_inside.push_back(body)
+	(body as Animal).enter_safe_area()
+	if animals_inside.size() == animals.size():
+		print("yippee!")
+
+func _on_body_exited(body: Node2D) -> void:
+	if not (body in animals_inside):
+		return
+	
+	(body as Animal).retarget_safe_area()
+
+func is_complete() -> bool:
+	return complete
