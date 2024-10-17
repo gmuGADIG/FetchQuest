049b5e1b99bfc36a29de566156850d550559397b
K <redknclarke@gmail.com>
2024-10-17 18:16:13 -0400

    set up amalgamation states

---
+++ enemy/amalgamation/states/amalgamation_sucking_state.gd
@@ -0,0 +1,10 @@
+class_name AmalgamationSuckingState extends AmalgamationState
+
+func enter() -> void:
+	pass
+
+func update(_delta:float) -> void:
+	pass
+
+func exit() -> void:
+	pass
---
+++ enemy/amalgamation/states/amalgamation_vulnerable_state.gd
@@ -0,0 +1,10 @@
+class_name AmalgamationVulnerableState extends AmalgamationState
+
+func enter() -> void:
+	pass
+
+func update(_delta:float) -> void:
+	pass
+
+func exit() -> void:
+	pass
