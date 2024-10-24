4f66f5ef2a6c73902b93bbfe8316750f98eef487
K <redknclarke@gmail.com>
2024-10-23 21:53:51 -0400

    amalgamation comments

--- enemy/amalgamation/states/amalgamation_spitting_state.gd
+++ enemy/amalgamation/states/amalgamation_spitting_state.gd
@@ -3,10 +3,12 @@ class_name AmalgamationSpittingState extends AmalgamationState
 @export var possible_enemies:Array[PackedScene]
 @export var spitting_number:int = 3
 func enter() -> void:
-	# Spit 
+	# Spit however much we want (with a delay)
 	for i in range(spitting_number):
 		spit()
 		await get_tree().create_timer(2).timeout
+	
+	# Idle after spitting all enemies
 	if state_machine.current_state != self:
 		return
 	state_machine.change_state(self, "Idle")
@@ -17,11 +19,14 @@ func update(_delta:float) -> void:
 func exit() -> void:
 	pass
 
-# Spawn a random enemy in the mouth
 func spit() -> void:
+	# Spawn a random enemy in the mouth
 	var enemy:Enemy = possible_enemies.pick_random().instantiate()	
 	enemy.global_position = %MouthArea.global_position
 	state_machine.get_parent().add_sibling(enemy)
+	
+	# Spit it out towards the player
 	var tween := create_tween()
 	var direction_to_player := enemy.global_position.direction_to(Player.instance.global_position)
 	tween.tween_property(enemy, "global_position", enemy.global_position + direction_to_player * 400, 1.5)
+	#NOTE idk why enemies dont move after being spat out but i think its not related to this
