aaabadd2575f0e6ca6ee57fc596d30bff22e9f57
Daringdeveloper25 <jhunt25@gmu.edu>
2024-10-30 21:20:27 -0400

    Added hurt function to player
    The player loses a life when he steps on a weak part of the floor. I still have yet to work on the floor opening up first.

--- world/node_2d.gd
+++ world/node_2d.gd
@@ -13,5 +13,6 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("Player")):
 		print("You stepped on a weak part of the floor!")
+		Player.instance.hurt(DamageEvent.new(1))
 		
 		
