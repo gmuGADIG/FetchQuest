825b26b97f3a44462bcb80b32176bd7285c32ef5
Mira <maclennanmira@gmail.com>
2024-10-30 20:14:04 -0400

    Add timer to sinking walls
    Currently the wall is set to stay down for 8 seconds.
    womp womp womp womp wompwompwomp

--- world/interactable/sinking-wall/sinking_wall.gd
+++ world/interactable/sinking-wall/sinking_wall.gd
@@ -4,17 +4,24 @@ extends StaticBody2D
 @onready var sunk := get_node("sunkenWall")
 @onready var hitbox := get_node("hitBox")
 
+#current wall timer is on 8 seconds
+var wallTime : float = 8.00
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	raiseWall()
+
+#Called when wall timer ends 
+func raiseWall() -> void:
 	notSunk.show()
 	sunk.hide()
 	set_collision_layer_value(1,true)
 	set_collision_layer_value(3,true)
-
-
+	
+	
 func hurt(_damage_event: DamageEvent) -> void:
 	notSunk.hide()
 	sunk.show()
 	set_collision_layer_value(1,false)
 	set_collision_layer_value(3,false)
-	pass
+	$wallTimer.start(wallTime)
