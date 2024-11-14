aeff39df3c1df01c4d91ddd22f7c92f553e8e2de
ElectroBlade0937 <sheldon.qtran@yahoo.com>
2024-11-13 21:12:36 -0500

    Update fetch_quest.gd
    Fetch quest can now support multiple item types.

--- quest/fetch_quest.gd
+++ quest/fetch_quest.gd
@@ -6,14 +6,14 @@ var item_types:Array[String] ## The type of item required to collect
 func _items_collected(index: int) -> bool:
 	return PlayerInventory.get_quantity(item_types[index]) >= number_of_required[index]
 
-func _on_item_updated() -> void:
+func _on_items_updated() -> void:
 	var check: bool = true
 	for i in range(len(number_of_required)):
 		if !(_items_collected(i)):
 			check = false
 	if check == true:
 		state = State.COMPLETED
-		PlayerInventory.item_updated.disconnect(_on_item_updated)
+		PlayerInventory.items_updated.disconnect(_on_items_updated)
 		QuestSystem.quest_completed.emit(self)
 
 func turn_in() -> void:
@@ -22,5 +22,5 @@ func turn_in() -> void:
 		PlayerInventory.remove_quantity(item_types[i], number_of_required[i])
 
 func _assign_hook() -> void:
-	PlayerInventory.item_updated.connect(_on_item_updated)
-	_on_item_updated()
+	PlayerInventory.items_updated.connect(_on_items_updated)
+	_on_items_updated()
