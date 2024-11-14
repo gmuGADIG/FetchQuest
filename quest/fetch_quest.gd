00b692091d2222c0f495f24792988be807a96ce4
ElectroBlade0937 <sheldon.qtran@yahoo.com>
2024-11-13 21:10:41 -0500

    Update fetch_quest.gd
    Fetch Quest can now support multiple item types.

--- quest/fetch_quest.gd
+++ quest/fetch_quest.gd
@@ -1,22 +1,26 @@
 class_name FetchQuest extends Quest
 
-var number_of_required:int ## The number of required items to complete the fetch_quest
-var item_type:String ## The type of item required to collect
+var number_of_required:Array[int] ## The number of required items to complete the fetch_quest
+var item_types:Array[String] ## The type of item required to collect
 
-func _items_collected() -> bool:
-	return PlayerInventory.get_quantity(item_type) >= number_of_required
+func _items_collected(index: int) -> bool:
+	return PlayerInventory.get_quantity(item_types[index]) >= number_of_required[index]
 
-func _on_item_updated(key: String) -> void:
-	if key == item_type:
-		if _items_collected():
-			state = State.COMPLETED
-			PlayerInventory.item_updated.disconnect(_on_item_updated)
-			QuestSystem.quest_completed.emit(self)
+func _on_item_updated() -> void:
+	var check: bool = true
+	for i in range(len(number_of_required)):
+		if !(_items_collected(i)):
+			check = false
+	if check == true:
+		state = State.COMPLETED
+		PlayerInventory.item_updated.disconnect(_on_item_updated)
+		QuestSystem.quest_completed.emit(self)
 
 func turn_in() -> void:
 	super.turn_in()
-	PlayerInventory.remove_quantity(item_type, number_of_required)
+	for i in range(len(number_of_required)):
+		PlayerInventory.remove_quantity(item_types[i], number_of_required[i])
 
 func _assign_hook() -> void:
 	PlayerInventory.item_updated.connect(_on_item_updated)
-	_on_item_updated(item_type)
+	_on_item_updated()
