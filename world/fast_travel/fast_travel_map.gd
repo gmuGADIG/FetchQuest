416737bf3d258a12a1debd1fd7e57d1f7ddd0690
Caleb-Augustine <Calebaugustine117@gmail.com>
2024-11-20 20:33:41 -0500

    Technically Fixed Bug
    Technically the pause menu does open over the fast travel menu, but the fast travel button now doesn't work.
    Also the game can't pause when the fast travel is open.

--- world/fast_travel/fast_travel_map.gd
+++ world/fast_travel/fast_travel_map.gd
@@ -1,11 +1,15 @@
 extends CanvasLayer
 
+#if we pause it here then we can't pause it when the pause menu comes up
 func _ready() -> void:
-	get_tree().paused = true;
+	#get_tree().paused = true;
+	pass
 
 func _on_tree_exit() -> void:
-	get_tree().paused = false;
+	#get_tree().paused = false;
+	pass
 
 func _on_resume_pressed() -> void:
-	get_tree().paused = false;
+	#get_tree().paused = false;
 	queue_free();
+	#pass
