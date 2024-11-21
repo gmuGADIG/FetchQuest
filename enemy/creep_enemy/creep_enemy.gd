8893c79c1f8bf45e538acd8650bcc568ff2b77e9
Ethan Nguyen <72627687+cooki-chan@users.noreply.github.com>
2024-11-20 21:38:33 -0500

    creeper, aww man

--- enemy/creep_enemy/creep_enemy.gd
+++ enemy/creep_enemy/creep_enemy.gd
@@ -9,25 +9,14 @@ func _ready() -> void:
 	# Set up the spawning timer
 	$CreepSpawnTimer.wait_time = creep_spawn_time
 	$CreepSpawnTimer.start()
-
-# NOTE 	This movement code is taken from the test_enemy 
-#   	and will probably not stay like this forever
-func _physics_process(_delta: float) -> void:
-	if Player.instance == null: 
-		return
-
-	# Move towards and look at the player 
-	var movement_direction := (Player.instance.global_position - global_position).normalized()
-	velocity = movement_direction * 300#movement_speed
-	look_at(Player.instance.global_position)
-	move_and_slide()
 	
-
+func _process_agressive(delta: float) -> void:
+	enemy_state = EnemyState.ROAMING
 
 func _on_creep_spawn_timer_timeout() -> void:
 	if creep_prefab == null:
 		return
-	# Spawn creep when the timer runs out
+	# Spawn creep when the timer runs outsda
 	print("creep_enemy.gd: Spawned creep") 
 	var creep := creep_prefab.instantiate()
 	creep.position = get_parent().to_local(self.global_position)
