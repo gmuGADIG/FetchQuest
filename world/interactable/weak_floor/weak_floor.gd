8d7ab8d1e8cf7f5b5a38a129f02f511b573623f0
ben <bwall4@gmu.edu>
2024-12-04 21:46:33 -0500

    Mostly fix weak floor

--- world/interactable/weak_floor/weak_floor.gd
+++ world/interactable/weak_floor/weak_floor.gd
@@ -26,8 +26,7 @@ func _set_alive_and_reset_timer(new_alive: bool) -> void:
 	alive = new_alive
 	_hole_occluder_collision.disabled = not alive
 	
-	if alive:
-		_sprite.modulate = Color.WHITE # .a = 1.0
+	_animate_decay(0.0 if alive else 1.0)
 		
 # Sets the "decay" animation to a specific "time" between 0 and 1. For now
 # just changes the modulate.
@@ -35,9 +34,18 @@ func _animate_decay(how_decayed: float) -> void:
 	const color_good = Color.WHITE
 	const color_decay = Color.DIM_GRAY
 	_sprite.modulate = color_good.lerp(color_decay, how_decayed)
+	
+	if how_decayed >= 1.0:
+		hide()
+	else:
+		show()
 
 func _process(delta: float) -> void:
 	if alive:
+		# When we're alive, set alpha based on how much time remains.
+		# This is done before the timer reset to avoid a visual glitch.
+		_animate_decay(timer / safe_length)
+		
 		if _area_2d.has_overlapping_bodies():
 			timer += delta
 			if timer >= safe_length:
@@ -45,10 +53,11 @@ func _process(delta: float) -> void:
 		else:
 			# Have the timer tick back down when nothing is on the platform.
 			timer = max(timer - delta, 0)
-			
-		# When we're alive, set alpha based on how much time remains.
-		_animate_decay(timer / safe_length)
+		
 	else:
 		timer += delta
+		# ALways look fully decayed.
+		_animate_decay(1.0)
 		if timer >= respawn_time:
 			_set_alive_and_reset_timer(true)
+	
