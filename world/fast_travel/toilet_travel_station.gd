eb8386030d54eb3e4143e156f3e98662c3ed1fef
KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>
2024-12-30 13:28:04 -0500

    Controller on Toilet UI
    The ToiletTravel UI is now navigable using a controller.
    
    The location that the menu is entered from is what is first selected

--- world/fast_travel/toilet_travel_station.gd
+++ world/fast_travel/toilet_travel_station.gd
@@ -5,5 +5,8 @@ extends Area2D
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and Player.instance in get_overlapping_bodies():
 		var scene_name := get_tree().current_scene.scene_file_path.trim_suffix(".remap").trim_suffix(".tscn").get_file()
-		FastTravelPoints.add_to_travel_points(TravelPoint.new(scene_name, entry_point))
-		add_sibling(preload("res://ui/fast_travel_map/fast_travel_map.tscn").instantiate())
+		var travel_point : TravelPoint = TravelPoint.new(scene_name, entry_point)
+		FastTravelPoints.add_to_travel_points(travel_point)
+		var map : FastTravelMap = preload("res://ui/fast_travel_map/fast_travel_map.tscn").instantiate()
+		add_sibling(map)
+		map.entered_at(travel_point)
