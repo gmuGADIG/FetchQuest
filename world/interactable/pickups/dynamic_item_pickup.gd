1861bdeead531bbc6ee31b63a49c63cde83afce2
Justin1L8 <justin.w.langdon@gmail.com>
2025-01-05 14:14:10 -0500

    Refactored single-use items to be handled in the base Item class

--- world/interactable/pickups/dynamic_item_pickup.gd
+++ world/interactable/pickups/dynamic_item_pickup.gd
@@ -6,25 +6,15 @@ class_name DynamicItemPickup extends Item
 ## When collected, the value is incremented by this amount
 @export var amount := 1
 
-## By default, collected items respawn when the scene is loaded.
-## If true, this is prevented, making this item only able to be picked up one time.
-@export var single_use := false
-
-## Array of node paths, each corresponding to the path of a single-use item that's been collected already
-static var collected_single_use_items: Array[NodePath] = []
-
 func _ready() -> void:
-	if single_use and collected_single_use_items.has(get_path()):
-		queue_free()
-		return
-	
+	super._ready()
 	var node_display_name := get_parent().name+"/"+name
 	assert(inventory_key != "", "Item '%s' has an empty inventory key!" % node_display_name)
 	assert(not inventory_key in PlayerInventory, "Item '%s' has key '%s', which is a built-in property of PlayerInventory. You probably want to use the `property_item_pickup` script instead." % [node_display_name, inventory_key])
 
 func consume(consumer: Node2D) -> void:
+	super.consume(consumer)
 	if (consumer is Player):
 		PlayerInventory.add_quantity(inventory_key, amount)
 		print("key_item_pickup.gd: Picked up item '%s'" % inventory_key)
-		if single_use: collected_single_use_items.append(get_path())
 		queue_free()
